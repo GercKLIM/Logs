6a708039071ce5abe1c2c50644da4bbd63ae380c
GercKLIM <gerceg0808@yandex.ru>
2024-11-26 15:10:02 +0300

    Create project!

--- tests/LogsTest.cpp
+++ tests/LogsTest.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 int main() {
-
+    // Ввести в shell на Windows для цветного вывода:
+    // reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1
     std::cout << Logs::LOG_SUCCESS << "Complete!"  << std::endl;
     std::cout << Logs::LOG_FACT    << "Complete!"  << std::endl;
     std::cout << Logs::LOG_ALERT   << "Complete!"  << std::endl;
